4ca4c58b35027dceab143b1d0d7a9337f91aac3b
Aimee Soltys <aimee.soltys@gmail.com>
2016-05-14 21:54:38 -0500

    Add Current Status pages and Village page

---
+++ MobileApp/HmongTrail/HmongTrail/Pages/VillageImage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace HmongTrail.Pages
+{
+    public partial class VillageImage : ContentPage
+    {
+        public VillageImage()
+        {
+            InitializeComponent();
+        }
+    }
+}
