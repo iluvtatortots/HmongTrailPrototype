0d231fdda2834481ec52e30ddde118f6e4d19f07
Aimee Soltys <aimee.soltys@gmail.com>
2016-05-15 00:36:47 -0500

    Updated Credits page to include historical references. Add General Store page, CurrentStatusGood page, and CurrentStatusBad page.

--- MobileApp/HmongTrail/HmongTrail/PageModels/CurrentStatusGoodPageModel.cs
+++ MobileApp/HmongTrail/HmongTrail/PageModels/CurrentStatusGoodPageModel.cs
@@ -1,13 +1,26 @@
 ﻿using FreshMvvm;
+using HmongTrail.Models;
+using PropertyChanged;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace HmongTrail.PageModels
 {
+    [ImplementPropertyChanged]
     public class CurrentStatusGoodPageModel : FreshBasePageModel
     {
+        public Command ContinueTravelingCommand
+        {
+            get
+            {
+                return new Command(() => {
+                    CoreMethods.PushPageModel<CurrentStatusBadPageModel>();
+                });
+            }
+        }
     }
 }
--- MobileApp/HmongTrail/HmongTrail/PageModels/GeneralStorePageModel.cs
+++ MobileApp/HmongTrail/HmongTrail/PageModels/GeneralStorePageModel.cs
@@ -1,14 +1,26 @@
 ﻿using FreshMvvm;
+using HmongTrail.Models;
+using PropertyChanged;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace HmongTrail.PageModels
 {
+    [ImplementPropertyChanged]
     public class GeneralStorePageModel : FreshBasePageModel
     {
-
+        public Command BeginTravelingCommand
+        {
+            get
+            {
+                return new Command(() => {
+                    CoreMethods.PushPageModel<CurrentStatusGoodPageModel>();
+                });
+            }
+        }
     }
 }
